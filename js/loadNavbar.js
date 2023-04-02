6d6343508b54bd567b7f20fd64937a84c4b1999b
CezarPP <cezarpopoveniuc51@gmail.com>
2023-04-02 17:54:15 +0300

    Added content to help and about pages

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,9 +1,10 @@
-const loadNavbar = async () => {
+async function loadNavbar() {
     try {
         const response = await fetch('/pages/navbar.html');
         document.getElementById('navbar-container').innerHTML = await response.text();
     } catch (error) {
         console.error('Error fetching navbar:', error);
     }
-};
+}
+
 loadNavbar();
