d6ef374b0f82c5465bb4bc19bdc5a0d7984fbffb
CezarPP <cezarpopoveniuc51@gmail.com>
2023-04-08 18:39:50 +0300

    Modified nav bar to not have search and be more responsive

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -7,4 +7,17 @@ async function loadNavbar() {
     }
 }
 
-loadNavbar();
+loadNavbar().then(r => {
+        console.log("Added nav bar");
+        const checkbox = document.getElementById("check");
+        checkbox.addEventListener("change", function () {
+            console.log("Entered event listener");
+            if (checkbox.checked) {
+                document.body.classList.add("no-scroll");
+            } else {
+                document.body.classList.remove("no-scroll");
+            }
+        });
+        console.log("added event listener");
+    }
+)
