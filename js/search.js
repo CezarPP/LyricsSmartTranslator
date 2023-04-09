822fddec05e34313fded4a4b00c4a0b6d59e325e
CezarPP <cezarpopoveniuc51@gmail.com>
2023-04-10 01:47:38 +0300

    Modified search page so that it doesn't display searched text with no parameters

--- js/search.js
+++ js/search.js
@@ -1,3 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get('q');
-document.getElementById('searched-text').innerText = `"${query}"`;
+if (query !== null) {
+    document.getElementById('searched-text').innerText = `"${query}"`;
+} else {
+    document.getElementById('searched-text').style.display = 'none';
+    document.getElementById('search-results-title').style.display = 'none';
+}
