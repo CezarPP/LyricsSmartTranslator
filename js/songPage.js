2e67617d7eb0cb5d615efaaf1cb6fa8008372c81
unknown <vladmihainechita@gmail.com>
2023-04-10 00:27:41 +0300

    Aliniere Comentarii

--- js/songPage.js
+++ js/songPage.js
@@ -128,19 +128,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    /*https://img.olympicchannel.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/eegjuvlm3u2lrhkcc1oc*/
     function addComment(text) {
         const comment = document.createElement("div");
         comment.classList.add("comment");
 
+        const infAuthor = document.createElement("img");
+        infAuthor.src = "https://img.olympicchannel.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/eegjuvlm3u2lrhkcc1oc";
+        infAuthor.alt = "Avatar Image";
+        infAuthor.width = "60";
+        infAuthor.height = "45";
+        comment.appendChild(infAuthor);
+
+        const commentDetails = document.createElement("div");
+        commentDetails.classList.add("comment-details");
+
         const commentAuthor = document.createElement("h3");
         commentAuthor.textContent = "Ionut";
-        comment.appendChild(commentAuthor);
+        commentDetails.appendChild(commentAuthor);
 
         const commentText = document.createElement("p");
         commentText.textContent = text;
-        comment.appendChild(commentText);
+        commentDetails.appendChild(commentText);
+
+        comment.appendChild(commentDetails);
 
         commentsList.appendChild(comment);
     }
+
 });
 
