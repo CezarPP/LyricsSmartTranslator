ab24412a2e8cfd85c6ab574666c98c3cda0c3417
CezarPP <cezarpopoveniuc51@gmail.com>
2023-04-10 03:55:49 +0300

    Added the ability for semi-persistent annotations, editing and seeing previous annotations

--- js/song-page-annotations.js
+++ js/song-page-annotations.js
@@ -2,17 +2,45 @@ document.addEventListener('DOMContentLoaded', () => {
     // annotations feature
     let activeAnnotationBox;
     const lyricsContainer = document.querySelector('.lyrics-container');
-    const annotationsContainer = document.querySelector('.annotations-container');
+
+    const annotationsMap = new Map(); // (text, annotation)
 
     lyricsContainer.addEventListener('mouseup', () => {
         const selection = window.getSelection();
         const selectedText = selection.toString();
-        if (selectedText) {
-            showAnnotationBox(selection);
+        if (selectedText && !annotationsMap.has(selectedText)) {
+            showAnnotationBox(selection, selectedText);
         }
     });
 
-    function showAnnotationBox(selection) {
+    document.addEventListener('mousedown', (event) => {
+        hideAnnotationsOnClick(event);
+    });
+
+    function hideAnnotationsOnClick(event) {
+        const annotationBoxes = document.querySelectorAll('.annotation-box-active');
+        if (annotationBoxes.length === 0) {
+            return;
+        }
+        let shouldHideAnnotations = true;
+        annotationBoxes.forEach((box) => {
+            if (box.contains(event.target)) {
+                shouldHideAnnotations = false;
+            }
+        });
+        if (shouldHideAnnotations) {
+            annotationBoxes.forEach((box) => {
+                box.classList.remove('annotation-box-active');
+            });
+        }
+    }
+
+    /**
+     * Prints the box that lets you annotate text not annotated before
+     * @param selection
+     * @param selectedText
+     */
+    function showAnnotationBox(selection, selectedText) {
         const range = selection.getRangeAt(0);
         const rect = range.getBoundingClientRect();
         const annotations = document.getElementById("annotations-container").getBoundingClientRect();
@@ -29,11 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const textArea = document.createElement('textarea');
         textArea.id = "annotations-input";
         textArea.placeholder = "Write your comment here";
-        /*
-            <label for="comment-input"></label>
-            <textarea id="comment-input" placeholder="Write your comment here"></textarea>
-             <button id="submit-comment">Submit</button>
-         */
 
         const btnSubmit = document.createElement('button');
         btnSubmit.id = 'submit-annotation';
@@ -46,46 +69,21 @@ document.addEventListener('DOMContentLoaded', () => {
         btnWrapper.id = 'annotation-btn-wrapper';
 
         btnSubmit.addEventListener('click', () => {
-            alert('Added annotation');
             let annotationText = textArea.value.trim();
             if (annotationText) {
-                const annotationElement = document.createElement('div');
-                annotationElement.classList.add('annotation');
-                annotationElement.innerHTML = `
-                            <h3>${selection.toString()}</h3>
-                            <p>${annotationText}</p>
-                        `;
-                annotationsContainer.appendChild(annotationElement);
+                annotationsMap.set(selectedText, annotationText);
+                const span = document.createElement('span');
+                span.style.backgroundColor = 'rgb(233, 233, 233)';
+                span.style.cursor = 'pointer';
+                span.textContent = selectedText;
+                span.addEventListener('click', () => {
+                    showAnnotatedBox(span);
+                });
+                range.deleteContents();
+                range.insertNode(span);
                 box.remove();
             }
         });
-
-        document.addEventListener('mousedown', (event) => {
-            hideAnnotationsOnClick(event);
-        });
-
-        function hideAnnotationsOnClick(event) {
-            const annotationBoxes = document.querySelectorAll('.annotation-box-active');
-
-            if (annotationBoxes.length === 0) {
-                return;
-            }
-
-            let shouldHideAnnotations = true;
-
-            annotationBoxes.forEach((box) => {
-                if (box.contains(event.target)) {
-                    shouldHideAnnotations = false;
-                }
-            });
-
-            if (shouldHideAnnotations) {
-                annotationBoxes.forEach((box) => {
-                    box.classList.remove('annotation-box-active');
-                });
-            }
-        }
-
 
         const annotationBoxTitle = document.createElement('h2');
         annotationBoxTitle.textContent = 'Annotation';
@@ -108,16 +106,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 0);
     }
 
-    function addAnnotation(selectedText, annotation) {
-        const annotationElement = document.createElement('div');
-        annotationElement.classList.add('annotation');
-        annotationElement.innerHTML = `
-            <h3>${selectedText}</h3>
-            <p>${annotation}</p>
-        `;
-        annotationsContainer.appendChild(annotationElement);
-    }
-
     function updateAnnotationBoxPosition() {
         if (activeAnnotationBox) {
             const range = window.getSelection().getRangeAt(0);
@@ -130,5 +118,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('resize', updateAnnotationBoxPosition);
+
+    /**
+     * Prints the box that lets you edit already annotated text
+     * @param span -> the span that contains the text that was clicked on
+     */
+    function showAnnotatedBox(span) {
+        const rect = span.getBoundingClientRect();
+        const annotations = document.getElementById("annotations-container").getBoundingClientRect();
+        const box = document.createElement('div');
+        box.classList.add('annotation-box', 'annotation-box-active');
+        const topValue = rect.top + window.scrollY;
+        box.style.left = `${annotations.left}px`;
+        box.style.top = `${topValue}px`;
+        activeAnnotationBox = box;
+
+        const annotationBoxTitle = document.createElement('h2');
+        annotationBoxTitle.textContent = 'Annotation';
+        annotationBoxTitle.classList.add('annotation-box-title');
+
+        const textArea = document.createElement('textarea');
+        textArea.id = "editable-annotation-text";
+        textArea.readOnly = true;
+        textArea.value = annotationsMap.get(span.textContent);
+
+        const btnEdit = document.createElement('button');
+        btnEdit.id = 'btn-edit-annotation';
+        btnEdit.textContent = 'Edit';
+
+        const btnSave = document.createElement('button');
+        btnSave.id = 'btn-save-annotation';
+        btnSave.textContent = 'Save';
+
+        const btnShare = document.createElement('button');
+        btnShare.id = 'btn-share-annotation';
+        btnShare.textContent = 'Share';
+
+        const btnWrapper = document.createElement('div');
+        btnWrapper.id = 'annotation-btn-wrapper';
+
+        btnEdit.addEventListener('click', () => {
+            textArea.readOnly = !textArea.readOnly;
+        });
+
+        btnSave.addEventListener('click', () => {
+            const newText = textArea.value.trim();
+            annotationsMap.set(span.textContent, newText);
+            textArea.readOnly = !textArea.readOnly;
+        });
+
+        btnShare.addEventListener('click', () => {
+            alert("Sharing is caring");
+        });
+
+        box.appendChild(annotationBoxTitle);
+        box.appendChild(textArea);
+        btnWrapper.appendChild(btnEdit);
+        btnWrapper.appendChild(btnSave);
+        btnWrapper.appendChild(btnShare);
+        box.appendChild(btnWrapper);
+
+        document.body.appendChild(box);
+    }
 });
 
