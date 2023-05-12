4d20ebb3c2b976ff54007294d1acf7ecd3192e11
CezarPP <cezarpopoveniuc51@gmail.com>
2023-05-12 14:12:13 +0300

    Hello world server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const hostname: string = '127.0.0.1';
+const port: number = 3000;
+
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
