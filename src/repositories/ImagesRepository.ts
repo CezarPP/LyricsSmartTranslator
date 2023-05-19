578c6977ac8d9afdb18a61dcfdd88db98f08d504
CezarPP <cezarpopoveniuc51@gmail.com>
2023-05-19 19:10:04 +0300

    Images to cloud repo

--- src/repositories/ImagesRepository.ts
+++ src/repositories/ImagesRepository.ts
@@ -1,10 +1,15 @@
 import {Pool} from 'pg';
 import {ImageModel} from '../models/ImageModel';
+import {Storage} from '@google-cloud/storage';
+import {v4 as uuidv4} from 'uuid';
 
 export class ImagesRepository {
     private db: Pool;
+    private storage: Storage;
+    private bucketName = 'images-post';
 
     constructor() {
+        this.storage = new Storage();
         this.db = new Pool({
             user: 'ionut',
             host: 'dpg-chf53k2k728trctjdjtg-a.frankfurt-postgres.render.com',
@@ -17,26 +22,63 @@ export class ImagesRepository {
         });
     }
 
-    async addImage(imageData: Buffer): Promise<number> {
-        const query = 'INSERT INTO images(img) VALUES($1) RETURNING id';
-        const values = [imageData];
+    async addImage(imageBuffer: Buffer): Promise<ImageModel | null> {
+        const bucket = this.storage.bucket(this.bucketName);
+        // either jpeg or png
+        const ext = await this.getExtension(imageBuffer);
+        if (ext == null) {
+            console.log("Not a valid image extension");
+            return null;
+        }
+        const contentType = `image/${ext}`;
+        const filename = `image-${uuidv4()}.${ext}`;
+        const file = bucket.file(filename);
 
         try {
+            await file.save(imageBuffer, {
+                contentType: contentType,
+            });
+
+            const imageUrl = `https://storage.googleapis.com/${bucket.name}/${filename}`;
+
+            // Insert the image details into the database
+            const query = 'INSERT INTO Images (link, extension) VALUES ($1, $2) RETURNING id';
+            const values = [imageUrl, ext];
             const result = await this.db.query(query, values);
-            return result.rows[0].id;
+
+            const insertedId = result.rows[0].id;
+            return new ImageModel(insertedId, imageUrl, ext);
         } catch (error) {
-            console.error(`Failed to insert image into database: ${error}`);
+            console.error(`Failed to upload image to Cloud Storage: ${error}`);
             throw error;
         }
     }
 
-    async getImage(id: number): Promise<ImageModel | null> {
-        try {
-            await this.db.connect();
-        } catch (error) {
-            console.error('Failed connection');
-            throw error;
+    private async getExtension(buffer: Buffer): Promise<string | null> {
+        // PNG: 137 80 78 71 13 10 26 10
+        const pngBytes = [137, 80, 78, 71, 13, 10, 26, 10];
+        const jpgStart = [255, 216];
+        const jpgEnd = [255, 217];
+        let isPng = true;
+        for (let i = 0; i < pngBytes.length; i++) {
+            if (pngBytes[i] !== buffer[i])
+                isPng = false;
         }
+        if (isPng)
+            return 'png';
+        let isJpg = true;
+        for (let i = 0; i < jpgStart.length; i++)
+            if (buffer[i] !== jpgStart[i]) {
+                isJpg = false;
+            }
+        if (buffer[buffer.length - 1] !== jpgEnd[1] || buffer[buffer.length - 2] !== jpgEnd[0])
+            isJpg = false;
+        if (isJpg)
+            return 'jpeg';
+        return null;
+    }
+
+    async getImageById(id: number): Promise<ImageModel | null> {
         const query = 'SELECT * FROM Images WHERE id = $1';
         const values = [id];
         const result = await this.db.query(query, values);
@@ -46,6 +88,6 @@ export class ImagesRepository {
         }
 
         const row = result.rows[0];
-        return new ImageModel(row.id, row.img);
+        return new ImageModel(row.id, row.link, row.extension);
     }
 }
