500b58922b73611c34fa5412cb70505a0c9091e6
CezarPP <cezarpopoveniuc51@gmail.com>
2023-05-19 12:26:14 +0300

    Fixed navbar and footer

--- src/public/js/loadFooter.js
+++ src/public/js/loadFooter.js
@@ -1,10 +1,11 @@
 async function loadFooter() {
     try {
-        const response = await fetch('footer.html');
+        const response = await fetch('/footer.html');
         document.getElementById('footer-container').innerHTML = await response.text();
     } catch (error) {
         console.error('Error fetching footer:', error);
     }
 }
 
-loadFooter();
+loadFooter().then(() =>
+    console.log("Loaded footer"));
--- src/public/js/loadNavbar.js
+++ src/public/js/loadNavbar.js
@@ -1,37 +1,13 @@
 async function loadNavbar() {
     try {
-        const response = await fetch('navbar.html');
+        const response = await fetch('/navbar.html');
         document.getElementById('navbar-container').innerHTML = await response.text();
     } catch (error) {
         console.error('Error fetching navbar:', error);
     }
 }
 
-loadNavbar().then(r => {
+loadNavbar().then(() => {
         console.log("Added nav bar");
-        const checkbox = document.getElementById("check");
-        const elementsChangeLinks = document.getElementsByClassName("change-links");
-        const featured = elementsChangeLinks[0];
-        const charts = elementsChangeLinks[1];
-        checkbox.addEventListener("change", function () {
-            console.log("Entered event listener");
-            if (checkbox.checked) {
-                document.body.classList.add("no-scroll");
-
-                for (let element of elementsChangeLinks) {
-                    element.onclick = function () {
-                        window.location.reload();
-                    }
-                    element.setAttribute("href", "/");
-                }
-            } else {
-                document.body.classList.remove("no-scroll");
-                featured.setAttribute("href", "/#news");
-                charts.setAttribute("href", "/#charts-container");
-                featured.removeAttribute("onclick");
-                charts.removeAttribute("onclick");
-            }
-        });
-        console.log("added event listener");
     }
 )
