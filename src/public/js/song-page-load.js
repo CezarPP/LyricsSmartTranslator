cb3c25626390eac7fee52f8ad716c168421f8c48
CezarPP <cezarpopoveniuc51@gmail.com>
2023-05-20 18:56:36 +0300

    Almost done song load

--- src/public/js/song-page-load.js
+++ src/public/js/song-page-load.js
@@ -1,7 +1,35 @@
+// TODO(add no_comments)
+// TODO(about should be a short description)
+
 async function getSongFromServer() {
     const path = window.location.pathname;
-    const songId = path.split('/')[2];
-    fetch(`/get-song-data/${songId}`, {method: 'POST'})
+    const translationId = path.split('/')[2];
+    fetch(`/api/translation/${translationId}`, {method: 'GET'})
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error getting translation from server status: ${response.status}`
+                    + `error is ${response.json()}`);
+            }
+            return response.json();
+        })
+        .then(translationData => {
+            loadTranslation(translationData);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+}
+
+async function loadTranslation(translationData) {
+    const songId = translationData.songId;
+    const userId = translationData.userId;
+    const language = translationData.language;
+    const description = translationData.description;
+    const lyrics = translationData.lyrics;
+    const no_views = translationData.no_views;
+    const time = translationData.time;
+
+    fetch(`/api/song/${songId}`, {method: 'GET'})
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`
@@ -9,28 +37,29 @@ async function getSongFromServer() {
             }
             return response.json();
         })
-        .then(data => {
-            loadSong(data);
+        .then(async data => {
+            await setSongElements(data.link, data.imageId, data.author, data.title);
         })
         .catch(error => {
             console.error('Error:', error);
         });
+
+    await setTranslationElements(lyrics, description, no_views);
 }
 
-async function loadSong(data) {
-    console.log("Link is " + data.songLink);
-    console.log("Lyrics are " + data.lyrics);
-    document.getElementById('song-link').src = data.songLink;
-    document.getElementById('song-img').src = data.imageLink;
-    document.getElementById('song-author').textContent = data.author;
-    document.getElementById('about-content').textContent = data.description; // TODO(change to short version)
-    document.getElementById('song-description').textContent = data.description;
-    document.getElementById('song-title').textContent = data.title;
-    document.getElementById('lyrics-song-title').textContent = data.title;
-    document.getElementById('lyrics-paragraphs').textContent = data.lyrics;
-    document.getElementById('no-likes').textContent = data.no_likes;
-    document.getElementById('no-comments').textContent = data.no_comments;
-    document.getElementById('no-views').textContent = data.no_views;
+async function setTranslationElements(lyrics, description, no_views) {
+    document.getElementById('about-content').textContent = description;
+    document.getElementById('song-description').textContent = description;
+    document.getElementById('lyrics-paragraphs').textContent = lyrics;
+    document.getElementById('no-views').textContent = no_views;
+}
+
+async function setSongElements(link, imageId, author, title) {
+    document.getElementById('song-link').src = link;
+    document.getElementById('song-img').src = imageLink;
+    document.getElementById('song-author').textContent = author;
+    document.getElementById('song-title').textContent = title;
+    document.getElementById('lyrics-song-title').textContent = title;
 }
 
 getSongFromServer().then(() => console.log("Got data from the server"));
