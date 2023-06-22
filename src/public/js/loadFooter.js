a3c45bb1069d0eb0520ab9f609692ef8710a2fa2
unknown <vladmihainechita@gmail.com>
2023-06-22 16:01:51 +0300

    CSV export

--- src/public/js/loadFooter.js
+++ src/public/js/loadFooter.js
@@ -2,10 +2,36 @@ async function loadFooter() {
     try {
         const response = await fetch('/footer.html');
         document.getElementById('footer-container').innerHTML = await response.text();
+
+        const csvIcon = document.querySelector('.csv-icon');
+        csvIcon.addEventListener('click', handleCSVIconClick);
     } catch (error) {
         console.error('Error fetching footer:', error);
     }
 }
+
+function handleCSVIconClick(event) {
+    event.preventDefault();
+
+    fetch('/all-songs', {
+        method: 'GET'
+    })
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'songs.csv';
+
+            link.click();
+            URL.revokeObjectURL(link.href);
+        })
+        .catch(error => {
+            console.error('Error fetching CSV file:', error);
+        });
+
+    console.log('CSV icon clicked');
+}
+
 
 loadFooter().then(() =>
     console.log("Loaded footer"));
